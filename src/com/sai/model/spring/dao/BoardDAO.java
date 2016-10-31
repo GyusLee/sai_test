4f381c73152c2190df96e19c71cdf9d773074a49
rick <lsb7151@gmail.com>
2016-10-31 17:30:26 +0900

    BoardDAO 수정/ BoardMapper, BoardDAOMybatis 제작

--- src/com/sai/model/spring/dao/BoardDAO.java
+++ src/com/sai/model/spring/dao/BoardDAO.java
@@ -9,5 +9,5 @@ public interface BoardDAO {
 	public Board selectOne(int board_id);
 	public int insert(Board board);
 	public int delete(int board_id);
-	public int update(int board_id);
+	public int update(Board board);
 }
--- src/com/sai/model/spring/dao/BoardDAOMybatis.java
+++ src/com/sai/model/spring/dao/BoardDAOMybatis.java
@@ -2,28 +2,38 @@ package com.sai.model.spring.dao;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.sai.model.domain.Board;
-
+@Repository
 public class BoardDAOMybatis implements BoardDAO{
-
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
 	public List selectAll() {
-		return null;
+		List list = sqlSessionTemplate.selectList("Board.selectAll");
+		return list;
 	}
 
 	public Board selectOne(int board_id) {
-		return null;
+		Board board = sqlSessionTemplate.selectOne("Board.selectOne", board_id);
+		return board;
 	}
 
 	public int insert(Board board) {
-		return 0;
+		sqlSessionTemplate.insert("Board.insert", board);
+		return board.getBoard_id();
 	}
 
 	public int delete(int board_id) {
-		return 0;
+		sqlSessionTemplate.delete("Board.delete", board_id);
+		return board_id;
 	}
 
-	public int update(int board_id) {
-		return 0;
-	}
-	
+	public int update(Board board) {
+		sqlSessionTemplate.update("Board.update", board);
+		return board.getBoard_id();
+	}	
 }
